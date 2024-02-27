f832105fea3e8fa9b4042c1c7fef5ee3e2e94d66
Amedamin <ahmedqryqr@gmail.com>
2024-02-27 09:15:52 -0800

    done

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -9,8 +9,8 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-        if (!root || !value)
-                return (NULL);
+	if (!root || !value)
+		return (NULL);
 
-        return (*root);
+	return (*root);
 }
