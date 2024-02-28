a8af29072b434a64c02f8dace8e7833bdb64f421
Amedamin <ahmedqryqr@gmail.com>
2024-02-27 21:30:28 -0800

    done

--- binary_trees.h
+++ binary_trees.h
@@ -78,4 +78,6 @@ heap_t *array_to_heap(int *array, size_t size);
 
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 int heap_extract(heap_t **root);
+
+avl_t *sorted_array_to_avl(int *array, size_t size);
 #endif
