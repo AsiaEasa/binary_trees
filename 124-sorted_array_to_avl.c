2b50b10870d69cf86e3154d7359fa549468ee43d
Amedamin <ahmedqryqr@gmail.com>
2024-02-27 21:39:25 -0800

    done

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -1,24 +1,5 @@
 #include "binary_trees.h"
 
-
-/**
- * binary_tree_levelorder - goes through a binary
- * @tree: pointer to the first element of the array to be converted
- * @size: the number of element in the array
- * Return: pointer to the root node of the created AVL tree, 
- */
-int binary_tree_levelorder(int *tree, size_t size)
-{
-	size_t H, L;
-
-	H = binary_tree_height(tree);
-
-	L = 0;
-	while (L <= H)
-	{
-		L++;
-	}
-}
 /**
  * sorted_array_to_avl - builds an AVL tree
  * @array: pointer to the first element
@@ -29,6 +10,4 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	if (!array || !size)
 		return (NULL);
-
-	return (binary_tree_levelorder(array,size));
 }
