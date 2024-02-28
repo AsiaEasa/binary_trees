e14530e535a72ab9f4f1def982e25e427d9d6672
AsiaEasa <asiaeasa89@gmail.com>
2024-02-27 20:50:34 -0800

    done

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -33,7 +33,7 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 	if (!heap || !size)
 		return (NULL);
 
-	*size = tree_size(heap) + 1;
+	*size = _tree_size(heap) + 1;
 
 	a = malloc(sizeof(int) * (*size));
 
